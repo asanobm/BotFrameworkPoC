b4ba5946b155c1e1f401a69bf786e2590fb572af
Asanobm <kim_bumin@fourglobe.co.jp>
2020-12-16 21:02:19 +0900

    add: UserProfile.cs

---
+++ Bots/SaveUserAndConversationData/UserProfile.cs
@@ -0,0 +1,13 @@
+namespace BotFrameworkPoC.Bots.SaveUserAndConversationData
+{
+    /// <summary>
+    /// User Profile class.
+    /// </summary>
+    public class UserProfile
+    {
+        /// <summary>
+        /// User Name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
