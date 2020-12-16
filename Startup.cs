753625d721ac328473866dd9d75adf197049069c
Asanobm <kim_bumin@fourglobe.co.jp>
2020-12-16 23:36:00 +0900

    * update: use StateManagementBot

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@
 // Generated with EchoBot .NET Template version v4.11.1
 
 using BotFrameworkPoC.Bots;
+using BotFrameworkPoC.Bots.StateManagementBot;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
@@ -16,9 +17,11 @@ namespace BotFrameworkPoC
 {
     public class Startup
     {
+        private MemoryStorage _storage;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _storage = new MemoryStorage();
         }
 
         public IConfiguration Configuration { get; }
@@ -30,13 +33,22 @@ namespace BotFrameworkPoC
 
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+            
+            // Create the user state passing in the storage layer.
+            var userState = new UserState(_storage);
+            services.AddSingleton(userState);
+            
+            // Create the Conversation state passing in the storage layer.
+            var conversationState = new ConversationState(_storage);
+            services.AddSingleton(conversationState);
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             
             // services.AddTransient<IBot, Bots.EchoBot>();
             // services.AddTransient<IBot, SendTypingIndicatorBot>();
             // services.AddTransient<IBot, AttachmentBot>();
-            services.AddTransient<IBot, ButtonBot>();
+            services.AddTransient<IBot, StateManagementBot>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
